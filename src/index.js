6ae62b2bca7aedfbde06504c63bc26aba7662859
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-18 17:22:07 +0000

    Current location option

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 
 
-function handleSearch (city) {
+function updateCity (city) {
     let timeElement = document.querySelector(".time");
-    
+    let cityElement = document.querySelector(".city");
 
-        timeElement.innerHTML= moment().tz(`${city}`).format("HH:mm:ss");
+    if (
+      city === "America/Los_Angeles" ||
+      city === "Africa/Harare" ||
+      city === "Europe/London" ||
+      city === "Asia/Tokyo"
+    ) {
+      cityElement.innerHTML = city.replace("_", " ").split("/")[1];
+      timeElement.innerHTML = moment().tz(`${city}`).format("HH:mm:ss A");
+    }
+    if (city === "current") {
+        cityElement.innerHTML = moment.tz.guess().replace("_", " ").split("/")[1];
+        let guessTimeZone = moment.tz.guess();
+        timeElement.innerHTML = moment.tz(guessTimeZone).format("HH:mm:ss A");
+    }
         
     
 }
 
-function updateCity (event) {
+function handleSearch (event) {
     event.preventDefault();
     let city = selectElement.value;
-    handleSearch(city);
-    if (city === "America/Los_Angeles") {
-    let cityElement = document.querySelector(".city");
-    cityElement.innerHTML ="Los Angeles";
-    }
+    updateCity(city);
 }
 
 let selectElement = document.querySelector(".select");
-selectElement.addEventListener("change", updateCity);
+selectElement.addEventListener("change", handleSearch);
 
 // setInterval(handleSearch);
