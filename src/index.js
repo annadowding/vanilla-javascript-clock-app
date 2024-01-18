b14c6e209821c31691e92aaf8f3f196bc947767c
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-18 16:21:53 +0000

    Set readable city name

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@
 
 function handleSearch (city) {
     let timeElement = document.querySelector(".time");
-    if (city === "Europe/London"){
+    
+
         timeElement.innerHTML= moment().tz(`${city}`).format("HH:mm:ss");
-    }
+        
+    
 }
 
 function updateCity (event) {
     event.preventDefault();
     let city = selectElement.value;
     handleSearch(city);
+    if (city === "America/Los_Angeles") {
+    let cityElement = document.querySelector(".city");
+    cityElement.innerHTML ="Los Angeles";
+    }
 }
 
 let selectElement = document.querySelector(".select");
 selectElement.addEventListener("change", updateCity);
+
+// setInterval(handleSearch);
